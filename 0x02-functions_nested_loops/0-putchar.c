85cca963bad8b34bf77aa722cdd06c2a45ea827d
ADEBAYO <adegbitequadriadebayo@gmail.com>
2022-03-15 20:09:27 +0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "putchar.c"
+/**
+ * main - prints Putchar
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('P');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\r');
+	return (0);
+}
