70e0520dde8a5980b39d6e1026bb802749b2054f
ADEBAYO <adegbitequadriadebayo@gmail.com>
2022-03-15 23:34:58 +0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,15 @@
 #include "stdio.h"
 
+
 /** 
- * *print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ * * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ *
  */
+
 void print_alphabet(void)
+
 {
+
 	char ch;
 
 	ch = 'a';
