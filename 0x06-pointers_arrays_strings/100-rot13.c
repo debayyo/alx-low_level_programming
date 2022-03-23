8e8c9488675b003f7fa8ac6ccfa9f1dce5200245
ADEBAYO <adegbitequadriadebayo@gmail.com>
2022-03-24 06:44:22 +0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,29 +1,36 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * rot13 - encodes a string using rot13
- * @s: input string
+ * rot13 - encode string using rot13
+ * @s: string to encode
  * Return: encoded string
  */
+
 char *rot13(char *s)
 {
-	int i;
 
-	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-	char *ptr = s;
+	int a[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		     'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		     'W', 'X', 'Y', 'Z'};
+	int b[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		     'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		     'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		     'J', 'K', 'L', 'M'};
 
-	while (*s)
+	int i, j;
+
+	for (j = 0; s[j] != '\0'; j++) /*loop through string*/
 	{
-		for (i = 0; i <= 52; i++)
-		{
-			if (*s == rot13[i])
-			{
-				*s = ROT13[i];
-				break;
-			}
-		}
-		s++;
+		i = 0;
+		while (a[i] != '\0' && s[j] != a[i]) /*loop through rot13 arr*/
+			i++;
+
+		if (s[j] == a[i]) /*if alpha matches, set to index in b arr*/
+			s[j] = b[i];
 	}
-	return (ptr);
+
+	return (s);
 }
