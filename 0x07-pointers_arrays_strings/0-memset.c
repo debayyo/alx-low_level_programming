41cf1fa4b4805513f1225acfbac3057494d4291d
ADEBAYO <adegbitequadriadebayo@gmail.com>
2022-03-28 19:20:08 +0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,10 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * *_memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
+ * @s: memory area
+ * @b: constant byte
+ * @n: bytes of the memory area
  *
  * Return: pointer to the memory area s
  */
