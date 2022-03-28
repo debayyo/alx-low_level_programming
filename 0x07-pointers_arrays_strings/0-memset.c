343813e1d991d2b3a63bc3d8350ad641b9a34062
ADEBAYO <adegbitequadriadebayo@gmail.com>
2022-03-28 19:11:21 +0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,9 +9,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	char *ptr = s;
+	unsigned int i;
 
-	while (n--)
-		*s+ = b;
-	return (ptr);
+	for (i = 0; i < n; i++)
+	{
+	
+		s[i] = b;
+	}
+
+	return (s);
 }
