ed709aa1525f7b912cb3b4dbda41772c2fb0d718
debayyo <adegbitequadriadebayo@gmail.com>
2022-04-26 18:07:13 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -16,13 +16,16 @@ size_t print_listint_safe(const listint_t *head)
 		diff = head - head->next;
 		num++;
 		printf("[%p] %d\n", (void *)head, head->n);
+
 		if (diff > 0)
 			head = head->next;
+
 		else
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			break;
 		}
 	}
+
 	return (num);
 }
